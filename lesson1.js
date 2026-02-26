befb5798d5d3c7e32492eecd5eb00138c824ab56
Brayemar <mogikoyob@gmail.com>
2026-02-26 10:51:16 +0300

    added java code

---
+++ lesson1.js
@@ -0,0 +1,3 @@
+console.log("Hello There")
+
+// The above function called consol.log() which is used to show us the out put on the output window
---
+++ lesson1c.js
@@ -0,0 +1,4 @@
+let height = 100
+let temperature = 40
+let points = 10
+console.log(height, temperature, points)
